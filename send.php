3dbaad7a7bb483ce76658f4bcd63c4ba1670e475
Aliaksandr <aleks_1994_1996@mail.ru>
2022-02-07 17:39:30 +0300

    diplom v2

--- send.php
+++ send.php
@@ -2,25 +2,31 @@
 $name = $_POST['name'];
 $tel = $_POST['tel'];
 
-$name = htmlspecialchars($name);
-$tel = htmlspecialchars($tel);
+// $name = htmlspecialchars($name);
+// $tel = htmlspecialchars($tel);
 
-$name = urldecode($name);
-$tel = urldecode($tel);
+// $name = urldecode($name);
+// $tel = urldecode($tel);
 
-$name = trim($name);
-$tel = trim($tel);
+// $name = trim($name);
+// $tel = trim($tel);
 
-if (mail("aleks_1994_1996@mail.ru",
+// if (mail("aleks_1994_1996@mail.ru",
+// "новое письмо",
+// "Имя: ".$name."\n".
+// "Телефон: ".$tel."\n".
+// "From: aleks.1994.1996@gmail.com \r\n"
+// )) {
+//   echo ('Письмо отправлено');
+// }
+
+// else {
+//   echo ('Ошибка при отправлении');
+// }
+mail("aleks_1994_1996@mail.ru",
 "новое письмо",
 "Имя: ".$name."\n".
 "Телефон: ".$tel."\n".
 "From: aleks.1994.1996@gmail.com \r\n"
-)) {
-  echo ('Письмо отправлено');
-}
-
-else {
-  echo ('Ошибка при отправлении');
-}
+)
 ?>
